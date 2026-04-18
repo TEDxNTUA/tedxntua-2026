629c0fd07b66ef1ea8c5fdde713a9d1499674a0a
Vakrinos Antonis <antonis.vakrinos@gmail.com>
2026-04-18 20:07:35 +0300

    small fix

--- app/event/experienceWorkshops/page.jsx
+++ app/event/experienceWorkshops/page.jsx
@@ -3,16 +3,15 @@ import AnchorScrollHandler from "../components/AnchorScrollHandler";
 import ExperienceInfoBox from "../components/WorkshopInfoBox";
 import EventPageShell from "../components/EventPageShell";
 
-import { myExperienceWorkshops1 } from "../infoDatabase";
+import { allExpWorkshops } from "../infoDatabase";
 
-const allWorkshops = [...myExperienceWorkshops1];
 
 export default function ProgramPage() {
   return (
     <EventPageShell title="Experience Workshops">
       <AnchorScrollHandler />
       <div className="grid grid-cols-1 justify-items-center gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {allWorkshops.map((item, index) => (
+        {allExpWorkshops.map((item, index) => (
           <ExperienceInfoBox key={index} {...item} />
         ))}
       </div>
