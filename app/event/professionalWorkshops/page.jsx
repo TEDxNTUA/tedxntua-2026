fa92748d3bda28973e647dde240744adb06f7fee
Vakrinos Antonis <antonis.vakrinos@gmail.com>
2026-04-19 10:53:57 +0300

    fix

--- app/event/professionalWorkshops/page.jsx
+++ app/event/professionalWorkshops/page.jsx
@@ -3,16 +3,14 @@ import AnchorScrollHandler from "../components/AnchorScrollHandler";
 import ProfessionalInfoBox from "../components/WorkshopInfoBox";
 import EventPageShell from "../components/EventPageShell";
 
-import { myProfessionalWorkshops1 } from "../infoDatabase";
-
-const allWorkshops = [...myProfessionalWorkshops1];
+import { allProfWorkshops } from "../infoDatabase";
 
 export default function ProgramPage() {
   return (
     <EventPageShell title="Professional Workshops">
       <AnchorScrollHandler />
       <div className="grid grid-cols-1 justify-items-center gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {allWorkshops.map((item, index) => (
+        {allProfWorkshops.map((item, index) => (
           <ProfessionalInfoBox key={index} {...item} />
         ))}
       </div>
