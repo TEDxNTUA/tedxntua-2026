ea17b11fde34a8e2028f327afc24df37ccd6fca5
Nikolaos Anagnostou <nikos.r.anagnostou@gmail.com>
2026-04-22 23:18:32 +0300

    Fix: Import missing useRef in AssetLoader

--- app/components/AssetLoader.jsx
+++ app/components/AssetLoader.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { usePathname } from "next/navigation";
 import { withBasePath } from "../lib/basePath";
 import localFont from "next/font/local";
