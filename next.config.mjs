af82be5e1a73deb53c4e6a9d6fe60b9c658aa7f5
piwzik <mishapiwz@gmail.com>
2026-04-24 18:49:46 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
-const configuredBasePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
-const basePath =
-  configuredBasePath || (isGithubActions && repositoryName ? `/${repositoryName}` : "");
+const hasConfiguredBasePath = Object.prototype.hasOwnProperty.call(
+  process.env,
+  "NEXT_PUBLIC_BASE_PATH",
+);
+const rawConfiguredBasePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+const configuredBasePath =
+  rawConfiguredBasePath === "/" ? "" : rawConfiguredBasePath.replace(/\/$/, "");
+const basePath = hasConfiguredBasePath
+  ? configuredBasePath
+  : configuredBasePath || (isGithubActions && repositoryName ? `/${repositoryName}` : "");
 
 const nextConfig = {
   output: "export",
