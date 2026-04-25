1243b5cb353b38b406209bb640bd165038387af6
Vakrinos Antonis <antonis.vakrinos@gmail.com>
2026-04-25 13:33:28 +0300

    !!! SideNav chenged to serve construction Page

--- app/components/EventSidebar.jsx
+++ app/components/EventSidebar.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const eventTabs = [
   { 
     label: "Program", 
-    path: "/event/program", 
+    path: "/event/constructionPage", 
     icon: (
       <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
